40290196ffa8dbca80ec9a39484eb7ea8d52227c
Your Name <nemanja.majstorovic3214@gmail.com>
2024-08-02 20:02:40 +0200

    Removed extra urdf

--- urdf/compile_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-rosrun xacro xacro crab_model.xacro > crab_model.urdf;
-#roslaunch urdf_tutorial display.launch model:=`rospack find urdf_demo`/urdf/crab_model.urdf gui:=true;
-rosrun gazebo_ros spawn_model -file `rospack find urdf_demo`/urdf/crab_model.urdf -urdf -x 0 -y 0 -z 1 -model crab 
